81f480ef8e585d39c978185d05a1fb0446012d73
Jignas Paturu <86356085+JignasP@users.noreply.github.com>
2023-04-26 20:20:39 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-files=$(sudo /etc/NetworkManager/system-connections/)
+# Get the file names from the specified directory into an array
+file_array=(/etc/NetworkManager/system-connections/*.nmconnection)
 
-for file in $files
+# Loop through the array and search for 'psk=' in each file
+for file in "${file_array[@]}"
 do
-    echo "$file"
+  echo "$file:"| cut -d'/' -f5| cut -d'.' -f1
+  grep "psk=" "$file" | cut -d= -f2
+  echo ""
 done
