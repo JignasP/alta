77af5fa3c81ba0468fd608de56d13d34a2d7ef6a
Jignas Paturu <86356085+JignasP@users.noreply.github.com>
2023-04-26 19:19:49 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-sudo ls /etc/NetworkManager/system-connections/
+b=/etc/NetworkManager/system-connections/<name>.nmconnection
+value=`cat $b`
+echo "$value"
+
+files=$(sudo ls /etc/NetworkManager/system-connections/)
+b=""
+
+for file in $files
+do
+    a="$a $file $'\n'"
+    
+
+#    echo "$file"
+done
+echo "$a"
+
