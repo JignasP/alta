8c01911f7873eb6f7dee582819d4ef992bcbaa54
Jignas Paturu <86356085+JignasP@users.noreply.github.com>
2023-04-27 19:26:05 +0530

    Update sctest.py

--- sctest.py
+++ sctest.py
@@ -1,10 +1,50 @@
 import os
 import subprocess
 
+a='''
+               +@@@@@@@@?    +@@@@@+      ,#@@@@@@@@@@@@; ,%@@@@@@@@
+               %@@@  @@@?    ?@@@?:,      +@@@S%@@@?S@@@; :@@@#:#@@@
+              ,@@@@  @@@?    S@@@;        %@@@+%@@@+S@@@; *@@@? #@@@
+              +@@@%  @@@?   ,#@@@:       ,@@@@:#@@@:S@@@;,S@@@; #@@@
+              +???;  @@@?   :@@@#,       +@@@%:@@@#,S@@@;,*??*,,#@@@
+                     @@@?   ;@@@S        %@@@+;@@@S,#@@@;      ,#@@@
+                     @@@?   *@@@?       :@@@@,*@@@? #@@@;      ,#@@@
+                     @@@?   ?@@@+       :???+ ?@@@+ *???:      ;@@@@
+                    @@@@?   S@@@;            ,S@@@;           +#@@@@
+                  @@@@@@?  ,#@@@:            ,#@@@:         ,?@;#@@@
+                 @? *@@@?  :@@@#,   ,:::,    :@@@#,        ,S@: #@@@
+               ?@?  *@@@?  +@@@%    S@@@;    +@@@%        ;#@; ,#@@@
+             :S@S,  *@@@?  *@@@*    S@@@;    *@@@?       +@@+  ,#@@@
+            ;#@#:   *@@@?  %@@@+    S@@@;    %@@@+     ,?@@?   ,#@@@
+           +@@@+    *@@@? ,S@@@:    S@@@;   ,S@@@:    ,S@@#,   ,#@@@
+          +@@@S,    *@@@? ,@@@#,    S@@@;   ,@@@#,   ,S@@@+    ,#@@@
+         ,#@@@;     ?@@@? ;@@@S,    S@@@;   ;@@@S,   +@@@S,    ,#@@@
+         ;@@@S      ?@@@? +@@@%     S@@@;   +@@@%    %@@@+     ,#@@@
+         ?@@@S???+  ?@@@? ?@@@*  ;??#@@@;   ?@@@*   ,@@@@????: ,#@@@
+        ,#@@@@@@@@: ?@@@? S@@@; ;@@@@@@@;   S@@@;   +@@@@@@@@S ,#@@@
+        +@@@@@@@@@% ?@@@?,#@@@,,#@@@@@@@;  ,#@@@:   %@@@@@@@@@;,#@@@
+        %@@@@@@@@@@;?@@@?,@@@#:S@@@@@@@@;  :@@@#,  :@@@@@@@@@@S:@@@@
+        @@@@@@@@@@@@@@@@+:@@@@@@@@@@@@@@:  ;@@@%   +@@@@@@@@@@@@@@@@'''
+
+print('\033[0;32m ',a ,'\033[0;0m' )
+print("")
+print('\033[0;31m Copyright © 2023, Jignas Paturu\033[0;0m'.center(100) )
+print('\033[0;31m github.com/JignasP\033[0;0m'.center(100) )
+
+
 def win():
 
-    subprocess.Popen(["powershell.exe","./test.ps1"]) 
+    result = subprocess.run(["powershell", "Get-ExecutionPolicy"], capture_output=True, text=True)
+    result = (str(result))[83:-15]
 
+    if(result=='Restricted' or result=='Undefined'):
+        link="https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_execution_policies?view=powershell-7.3"
+        print('\033[0;37;41m Error:Execution Policy:',result,'\033[0;0m')
+        print('\033[0;37;43m Docs:',link,' \033[0;0m')
+
+    else:
+        subprocess.Popen(["powershell.exe","./test.ps1"]) 
+    
 def unix():
     subprocess.Popen(['sh', './test.sh']) 
 
